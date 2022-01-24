862f4aaa88803ae01bd6dc58691c9c135b43fe33
Iuliia El Sammak <juliag875@gmail.com>
2022-01-24 00:04:13 -0500

    commit tree f29333e7c78d7b94d4728e02b782ae9085e1d9b1

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  if (word === reversedWord) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  if the input is the same as reversed input
+    return true
+  else
+    false
 */
 
 /*
   Add written explanation of your solution here
+
+  input must be stricly equal reversed input
+  use split('') method to return new array
+  use reverse() method to reverse new created array
+  use join('') method to join all elements of the array into a string
+  use if else to check if input === reversed input
+
 */
 
 // You can run `node index.js` to view these console logs
